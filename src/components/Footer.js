ef37582656424b87e179aa558b4051d872e19a95
PawelPamula1 <pawelpamula003@gmail.com>
2021-12-04 21:55:16 +0100

    small change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
   return (
     <div className="text-center p-10 mt-10 border-t dark:border-gra-700 border-gray-200">
-      <h1>2021 Gugl, Inc.</h1>
+      <h1>2021 Gugl - by Paweł Pamuła</h1>
     </div>
   );
 };
