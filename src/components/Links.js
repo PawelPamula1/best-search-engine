b9f0de481224c5319c6a1795093edcdb5202b6f3
PawelPamula1 <pawelpamula003@gmail.com>
2021-12-05 15:22:56 +0100

    fixing problem

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useResultContext } from '../contexts/ResultContextProvider';
 
 const links = [
   { url: '/search', text: '🌍 All' },
@@ -9,13 +10,16 @@ const links = [
 ];
 
 export const Links = () => {
+  const { isLoading } = useResultContext();
+
   return (
     <div className="flex sm:justify-around justify-between items-center mt-4">
-      {links.map(({ url, text }) => (
-        <NavLink to={url} className="m-2 mb-0" activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">
-          {text}
-        </NavLink>
-      ))}
+      {!isLoading &&
+        links.map(({ url, text }) => (
+          <NavLink to={url} className="m-2 mb-0" activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">
+            {text}
+          </NavLink>
+        ))}
     </div>
   );
 };
